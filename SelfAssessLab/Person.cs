0fca04ad14d418f18df78d2153624d1cf50449bc
Jennifer Wenger <jennifer@techiewebdesigns.com>
2019-03-20 10:45:20 -0400

    Add files via upload

---
+++ SelfAssessLab/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfAssessLab
+{
+    public class Person
+    {
+        public Person(string name, int age, string address, string contactNo)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+            ContactNo = contactNo;
+
+
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string ContactNo { get; set; }
+    }
+}
